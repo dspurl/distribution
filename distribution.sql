8c22f84e02b415d6b1c77272c4d87e3ba2c14ca4
netxiao <netxiao@qq.com>
2020-12-05 11:43:33 +0800

    添加默认的注册奖励现金分销

--- distribution.sql
+++ distribution.sql
@@ -4,3 +4,8 @@ INSERT INTO `auth_rules` (`id`, `api`, `url`, `icon`, `title`, `pid`, `state`, `
 (null, 'CreateDistribution', '', '', '添加分销', 186, 0, 0),
 (null, 'EditDistribution', '', '', '分销编辑', 186, 0, 0),
 (null, 'DeleteDistribution', '', '', '分销删除', 186, 0, 0);
+
+INSERT INTO `distributions` (`name`, `identification`, `level`, `state`) VALUES
+('注册奖励现金', 'sys_registration_cash', 1, 0);
+INSERT INTO `distribution_rules` (`distribution_id`, `name`, `type`, `level`, `price`) VALUES
+(1, '1级分销', 0, 1,1000);
